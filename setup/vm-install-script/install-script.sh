0653a1fc3bdb55cdcc1a14238c8950195765e35e
Sarmistha_Jena <sarmistha_jena@epam.com>
2023-03-12 18:42:36 +0530

    Updating kubernetes version

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -61,7 +61,7 @@ kubectl get node -o wide
 
 
 echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
-sudo apt install openjdk-8-jdk -y
+sudo apt install default-jre
 java -version
 sudo apt install -y maven
 mvn -v
